29bc3eca5c40cd54ed391a604e07f7d66eeed523
Jared Maldonado <jaredmaldonado@hotmail.com>
2023-01-24 21:26:05 -0600

    adding links to youtube

--- src/assets/main.js
+++ src/assets/main.js
@@ -19,18 +19,21 @@ async function fetchData(urlApi) {
 (async () => {
   try {
     const videos = await fetchData(API);
+    let vidUrl = 'https://www.youtube.com/watch?v=' + videos.items.map(video => video.id.videoId);
     let view = `
       ${videos.items.map(video => `
         <div class="group relative">
           <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full"><a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank"></a>
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
           </div>
           <div class="mt-4 flex justify-between">
-            <h3 class="text-sm text-aliceblue titles">
-              <span aria-hidden="true" class="absolute inset-0"></span>
-              ${video.snippet.title}
-            </h3>
+            <a href="${vidUrl}" target="_blank">
+              <h3 class="text-sm text-aliceblue titles">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+              </h3>
+              </a>
           </div>
         </div>
         `).slice(0,4).join('')}
